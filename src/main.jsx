a9443db07f9e89e504cb4815220dbbdc9d78a506
Thinh <quachthinh82@gmail.com>
2023-07-22 16:04:18 +0700

    strictmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import GlobalStyles from '~/components/GlobalStyles'
@@ -5,10 +6,11 @@ import store from './app/store'
 import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <GlobalStyles>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </GlobalStyles>
+  <React.StrictMode>
+    <GlobalStyles>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </GlobalStyles>
+  </React.StrictMode>
 )
